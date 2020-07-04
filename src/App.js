520c9ad97b6981cf82e76c8f8d3b3f83b17ff56f
João Roberto <joaojrdev@gmail.com>
2020-07-04 16:16:19 -0300

    Added Docker

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vamo learn react 2
         </a>
       </header>
     </div>
